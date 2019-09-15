2180cdea7c28c35b0db0cefa5c5492827718aac4
gikap <kovavka@gmail.com>
2019-09-16 04:07:23 +0500

    add hand service

---
+++ src/utils/HandService.ts
@@ -0,0 +1,87 @@
+import {SuitType, Tile} from "../models/Tile";
+import {WallGenerator} from "./WallGenerator";
+
+export class HandService {
+    wall: Tile[] = []
+    hand: Tile[] = []
+    discard: Tile[] = []
+    tsumo: Tile | undefined
+
+    generate(): Tile[]  {
+        let wall = WallGenerator.generate()
+
+        this.hand = wall.slice(0,13).sort(this.sortHandler)
+        this.wall = wall.slice(13)
+        this.discard = []
+
+        return this.getHand()
+    }
+
+    getHand(): Tile[] {
+        return this.hand.slice(0)
+    }
+
+    getStr(): string {
+        let hand = ''
+
+        let man = this.hand.filter(x => x.suit === SuitType.MANZU)
+        if (man.length) {
+            hand += man.map(x => x.value).join('') + 'm'
+        }
+
+        let pin = this.hand.filter(x => x.suit === SuitType.PINZU)
+        if (pin.length) {
+            hand += pin.map(x => x.value).join('') + 'p'
+        }
+
+        let sou = this.hand.filter(x => x.suit === SuitType.SOUZU)
+        if (sou.length) {
+            hand += sou.map(x => x.value).join('') + 's'
+        }
+
+        let honors = this.hand.filter(x => x.suit === SuitType.JIHAI)
+        if (honors.length) {
+            hand += honors.map(x => x.value).join('') + 'z'
+        }
+
+        return hand
+    }
+
+    nextTile(): Tile {
+        let tile = this.wall[0]
+        this.wall = this.wall.slice(1)
+        this.tsumo = tile
+        return tile
+    }
+
+    dropTsumo(): Tile[] {
+        if (this.tsumo) {
+            this.discard.push(this.tsumo)
+            this.tsumo = undefined
+        }
+
+        return this.getHand()
+    }
+
+    dropFromHand(index: number): Tile[] {
+        if (this.tsumo) {
+            let tile = this.hand[index]
+            this.discard.push(tile)
+
+            this.hand.splice(index, 1)
+            this.hand.push(this.tsumo)
+            this.hand = this.hand.sort(this.sortHandler)
+
+            this.tsumo = undefined
+        }
+
+        return this.getHand()
+    }
+
+    private sortHandler(a: Tile, b: Tile) {
+        if (a.suit == b.suit)
+            return a.suit - b.suit
+
+        return a.value - b.value
+    }
+}
