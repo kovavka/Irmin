352cc4780b09fa588eda4c84545f4f9d6b9638f5
Galina Kapger <kovavka@gmail.com>
2019-09-17 01:38:26 +0500

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {TempaiService} from "./utils/tempaiService";
+import {TempaiService} from "./utils/TempaiService";
 
 const App: React.FC = () => {
 
