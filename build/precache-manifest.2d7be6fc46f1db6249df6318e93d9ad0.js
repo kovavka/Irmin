01060f22d81984e9057d5950b313fdbffdd0a910
Galina Kapger <gikapger@gmail.com>
2021-10-06 22:56:48 +0500

    add build dir

---
+++ build/precache-manifest.2d7be6fc46f1db6249df6318e93d9ad0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7843675b72cf73242f7abbccb838507e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "939a2458c71baca2297d",
+    "url": "/static/css/main.d2874827.chunk.css"
+  },
+  {
+    "revision": "f26e74ee39cebd5d3ba9",
+    "url": "/static/js/2.ecad0866.chunk.js"
+  },
+  {
+    "revision": "939a2458c71baca2297d",
+    "url": "/static/js/main.dbbf23fe.chunk.js"
+  },
+  {
+    "revision": "973cdff448740d03dd2b",
+    "url": "/static/js/runtime~main.3d0c20d7.js"
+  }
+]);
